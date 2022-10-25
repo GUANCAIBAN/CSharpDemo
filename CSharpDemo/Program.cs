167cc92a12a2f04f867d375c934d2c03528dccab
GUANCAIBAN <l1242225261@gmail.com>
2022-10-25 18:58:09 +0800

    [+] add byte2file

---
+++ CSharpDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+namespace CSharpDemo
+{
+    class Demo1
+    {
+        public void byte2file() {
+            byte[] dllarray = new byte[] { 0x4d, 0x5a };
+            FileStream fsWrite = File.Create("file");
+            fsWrite.Write(dllarray, 0, dllarray.Length);
+
+        }
+    }
+    class executeDemo {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Demo1 D1 = new Demo1();
+            D1.byte2file();
+
+        }
+
+    }
+}
